1466733c295998f7270f7dc21aa8dfe9a5225425
Jurini <curvello_@hotmail.com>
2020-08-11 11:27:24 -0300

    Adicinando JS responsável pelas buscas

---
+++ js/buscar-pacientes.js
@@ -0,0 +1,21 @@
+var buscarPacientes = document.querySelector('#buscar-pacientes');
+
+buscarPacientes.addEventListener('click', function () {
+
+
+    var xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'https://api-pacientes.herokuapp.com/pacientes');
+
+    xhr.addEventListener('load', function () {
+        var resposta = xhr.responseText;
+        var pacientes = JSON.parse(resposta);
+
+        pacientes.forEach(function (paciente) {
+            adicionaPacienteNaTabela(paciente)
+        });
+    });
+
+    xhr.send();
+
+})
